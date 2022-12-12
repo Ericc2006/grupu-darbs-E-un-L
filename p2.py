a56a2def694334ae1b9d49139c5b468e8d79c721
Sanfekere <linards2006@gmail.com>
2022-12-12 11:52:36 +0200

    1213

--- p2.py
+++ p2.py
@@ -93,3 +93,56 @@ def aprekinashana(x, vaiirvainav, vaiirvainav2,vaiirvainav3,vaiirvainav4):
             print("Ievadi 4 istabas durvju izmerus")
             v = durvjuizm()
             return c+g+d+o+b+z+v
+
+def materiali(x):
+    global daudz, daudz1, daudz2
+    if x == 1:
+        material = int(input("""Izvēlies sienu materiāla tipu
+                                1 - Viniltapetes krāsojamās
+                                2 - Papīra tapetes
+                                """))
+        material1 = int(input("""Izvēlies griestu materiāla tipu
+                                1 - Griestuplāksnes
+                                2 - Nav
+                                """))
+        material2 = int(input("""Izvēlies grīdas materiāla tipu
+                                1 - Ūdensdrošs lamināts
+                                2 - Lamināts
+                                3 - Linolejs
+                                4 - Grīdas flīzes
+                                """))
+        if material == 1:
+            daudz = float(input("Ievadi viniltapešu daudzumu m2 - "))
+        elif material == 2:
+            daudz = float(input("Ievadi papīra tapešu daudzumu m2 - "))
+        if material1 == 1:
+            daudz1 = float(input("Ievadi Griestuplākšņu daudzumu m2 - "))
+        elif material1 == 2:
+            daudz1 = 0
+        if material2 == 1:
+            daudz2 = float(input("Ievadi Ūdensdrošs lamināta daudzumu m2 - "))
+        elif material2 == 2:
+            daudz2 = float(input("Ievadi Lamināta daudzumu m2 - "))
+        elif material2 == 3:
+            daudz2 = float(input("Ievadi Linoleja daudzumu m2 - "))
+        elif material2 == 4:
+            daudz2 = float(input("Ievadi Grīdas flīžu daudzumu m2 - "))
+
+        return material, material1, material2, daudz, daudz1, daudz2
+
+    elif x == 2:
+        material = int(input("""Izvēlies sienu materiāla tipu
+                                1 - Viniltapetes krāsojamās
+                                2 - Papīra tapetes
+                                """))
+        material1 = int(input("""Izvēlies griestu materiāla tipu
+                                1 - Griestuplāksnes
+                                2 - Nav
+                                """))
+        material2 = int(input("""Izvēlies grīdas materiāla tipu
+                                1 - Ūdensdrošs lamināts
+                                2 - Lamināts
+                                3 - Linolejs
+                                4 - Grīdas flīzes
+                                """))
+        return material, material1, material2
