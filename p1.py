4ef66fc06143498f417a41f5a5b6709d1da673f1
Ericc2006 <118733715+Ericc2006@users.noreply.github.com>
2022-12-12 11:46:55 +0200

    1

---
+++ p1.py
@@ -0,0 +1,5 @@
+def sienuaprekinasana():
+    siengar = float(input("Ievadi sienas garumu m - "))
+    sienplat = float(input("Ievadi sienas  platumu m - "))
+    sienaug = float(input("Ievadi sienas augstumu m - "))
+    return (siengar * sienaug) * 2 + (sienplat * sienaug) * 2
