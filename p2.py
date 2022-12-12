ad8c329c7f6261d8471d2df2f7300f532a0a9ba1
Sanfekere <linards2006@gmail.com>
2022-12-12 11:51:19 +0200

    123

---
+++ p2.py
@@ -0,0 +1,11 @@
+def loguundurvjizm():
+    loguaug = float(input("Ievadi loga augstumu m - "))
+    loguplat = float(input("Ievadi loga platumu m - "))
+    durvjuaug = float(input("Ievadi durvju augstumu m - "))
+    durvjuplat = float(input("Ievadi durvju platumu m - "))
+    return durvjuplat * durvjuaug + loguaug * loguplat
+
+def durvjuizm():
+    durvjuaug = float(input("Ievadi durvju augstumu m - "))
+    durvjuplat = float(input("Ievadi durvju platumu m - "))
+    return durvjuplat * durvjuaug
